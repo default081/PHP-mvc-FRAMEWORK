a637b272f77e275fd5e96cab7e3aecd4d5015ee9
bat <vadim.snake96@mail.ru>
2018-09-17 20:43:41 +0200

    init commit

--- Components/Router.php
+++ Components/Router.php
@@ -6,15 +6,23 @@ class Router{
 
   function __construct(){
       $this->routes = require_once("./Configs/routes.php");
-      echo "sex ";
   }
 
 
   function getUri(){
+      if(isset($_SERVER['REQUEST_URI'])){
+          $uri = preg_replace("~/framework/~", "", $_SERVER['REQUEST_URI']);
+          return $uri;
+      }  
   }
 
   function run(){
-      echo "d,mlvfmd;l";
+      $uri = $this->getUri();
+      foreach($this->routes as $uriPattern => $path){
+          if(preg_match("~$uriPattern~", $uri)){
+              echo 'index';
+          }
+      }
   }
 
 }
